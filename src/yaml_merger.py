7346cc6555c598070cde3a637c76687bc3aaf06a
Ayhan Eyikan <aeyikan@umd.edu>
2025-06-12 15:45:54 -0400

    Read yaml helper

--- src/yaml_merger.py
+++ src/yaml_merger.py
@@ -1,12 +1,30 @@
 #!/usr/bin/env python3
 
+import yaml
+
+
 
 # YAML REFERENCE STRUCTURE
-# $ref<#sub-field?>: <path-to-file?>#<path-to-object>
+# $replace<#sub-field?>: <path-to-file?>#<path-to-object>
+
+
+def read_yaml(file_path: str):
+    """
+    Read in given YAML file
+    """
+    try:
+        with open(file_path, 'r') as file:
+            yaml_content = yaml.safe_load(file)
+            return yaml_content
+    except FileNotFoundError:
+        print(f"Error: File not found at '{file_path}'")
+    except yaml.YAMLError as e:
+        print(f"Error: Could not parse YAML file. {e}")
+    return None
 
 
 def main():
-    pass
+    print(read_yaml('./tests/test.yaml'))
 
 
 if __name__ == '__main__':
